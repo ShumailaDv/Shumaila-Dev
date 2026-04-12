48419dc4d966d1016dd01a3b9700debfe42a98d1
Shumaila <codecanvas78@gmail.com>
2026-04-12 05:52:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // ================= LOADER =================
+const loader = document.getElementById("loader");
+
 window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
   loader.style.opacity = "0";
+  loader.style.transition = "0.5s ease";
 
   setTimeout(() => {
     loader.style.display = "none";
