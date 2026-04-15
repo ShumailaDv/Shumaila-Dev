bc50667401fc68a7f5ca95942867ae38afba4892
Shumaila <codecanvas78@gmail.com>
2026-04-14 20:45:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -148,7 +148,48 @@ if (window.particlesJS) {
     retina_detect: true
   });
 }
+// FILTER
+const filterButtons = document.querySelectorAll('.skill-filters button');
+const cards = document.querySelectorAll('.skill-card');
 
+filterButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    document.querySelector('.active').classList.remove('active');
+    btn.classList.add('active');
+
+    const filter = btn.getAttribute('data-filter');
+
+    cards.forEach(card => {
+      if (filter === 'all' || card.classList.contains(filter)) {
+        card.style.display = "flex";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+});
+
+
+// 3D TILT EFFECT
+cards.forEach(card => {
+  card.addEventListener('mousemove', (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / 10);
+    const rotateY = ((centerX - x) / 10);
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = "rotateX(0) rotateY(0)";
+  });
+});
 
 // ================= BOOK CALL =================
 function bookCall() {
