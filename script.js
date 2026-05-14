662f76fec9bb8c753fa1fdfe2ddd8eb2a71e8000
Shumaila <codecanvas78@gmail.com>
2026-05-14 03:29:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -134,6 +134,24 @@ if (window.particlesJS) {
     retina_detect: true
   });
 }
+
+<script>
+const revealElements = document.querySelectorAll(
+  '.about-image, .about-content-box'
+);
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.25
+});
+
+revealElements.forEach(el => observer.observe(el));
+</script>
 // FILTER
 const filterButtons = document.querySelectorAll('.skill-filters button');
 const cards = document.querySelectorAll('.skill-card');
@@ -185,6 +203,23 @@ function bookCall() {
     "noopener,noreferrer"
   );
 }
+<script>
+const revealElements = document.querySelectorAll(
+  '.about-image, .about-content-box'
+);
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.25
+});
+
+revealElements.forEach(el => observer.observe(el));
+</script>
 // ABOUT SECTION ANIMATION ON SCROLL
 const aboutBox = document.querySelector("#about .container");
 
