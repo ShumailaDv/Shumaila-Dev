d7febaf44585653d9f6a94da756f0cd5e5f769b1
Shumaila <codecanvas78@gmail.com>
2026-05-17 06:38:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -210,16 +210,10 @@ document.querySelector(".footer-text").innerHTML =
 
 //FRONTEND DEVELOPMENT
 
-const skillsSection = document.querySelector('#skills');
 const progressBars = document.querySelectorAll('.progress');
 
-window.addEventListener('scroll', () => {
-  const sectionPos = skillsSection.getBoundingClientRect().top;
-  const screenPos = window.innerHeight / 1.3;
-
-  if(sectionPos < screenPos){
-    progressBars.forEach(bar => {
-      bar.classList.add('animate');
-    });
-  }
+window.addEventListener('load', () => {
+  progressBars.forEach(bar => {
+    bar.classList.add('animate');
+  });
 });
